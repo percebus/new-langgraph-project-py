5b361ce2a0e21b38ec702dbdecda652ff4a34fa4
JCGuerrero <JCystems@gMail.com>
2025-11-03 15:40:42 -0600

    feat: client

---
+++ src/client/main.py
@@ -0,0 +1,23 @@
+# SRC: https://docs.langchain.com/oss/python/langgraph/local-server#7-test-the-api
+
+from langgraph_sdk import get_client
+import asyncio
+
+client = get_client(url="http://localhost:2024")
+
+async def main():
+    async for chunk in client.runs.stream(
+        None,  # Threadless run
+        "agent", # Name of assistant. Defined in langgraph.json.
+        input={
+        "messages": [{
+            "role": "human",
+            "content": "What is LangGraph?",
+            }],
+        },
+    ):
+        print(f"Receiving new event of type: {chunk.event}...")
+        print(chunk.data)
+        print("\n\n")
+
+asyncio.run(main())
